8a06978fadf4faf13ae13b143ee8aec8947c9bc5
Zack Windoloski <zwindoloski@gmail.com>
2014-01-27 10:29:28 -0500

    Add a StaticPages controller

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '9d50890d1458a4b62daa1bea5acdd5e89a2492e33430d6a425019793271da36d69b6fcf5d3fb1c16f8d3cc601fa132b4361c8c699c3b4cb64447a38291fb7bc3'
+SampleApp::Application.config.secret_token = 'd2342e1a04773c88994f58679a660ee9a78ab4d1e7640806273ecd8493e3820ffe69fd51fc08706a8e4e9c714a803dc68d1baf7dc2f23147e4277feb808b48b5'
---
+++ test/functional/static_pages_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get ..." do
+    get :...
+    assert_response :success
+  end
+
+end
