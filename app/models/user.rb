3d6ab32dd9be8a568b461ba6d102cf6084c79906
Zack Windoloski <zwindoloski@gmail.com>
2014-02-04 17:03:43 -0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name   
+  attr_accessible :email, :name, :password, :password_confirmation  
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
